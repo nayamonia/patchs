8d4245f5f03eb324f596271739a808f12e238a59
Gabriel Fernandes <gabriel@duel.com.br>
2017-08-19 11:25:05 -0300

    Script for install PHP 5.6.28 on ubuntu 12.04 LTS

---
+++ ub1204lts-install-php56.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Instalar PHP 5.6 no Ubuntu 12.04 LTS
+# Author: Gabriel Fernandes - gabriel@cd2.com.br
+
+apt-get update -y && apt-get install -y libmcrypt-dev
+wget http://br2.php.net/get/php-5.6.28.tar.gz/from/this/mirror -O php-5.6.28.tar.gz
+tar zxvf php-5.6.28.tar.gz && cd php-5.6.28
+./configure \
+    --enable-mbstring \
+    --with-zlib \
+	--enable-zip \
+	--with-gd \
+	--enable-soap \
+	--enable-sockets \
+	--enable-pcntl \
+	--enable-sigchild \
+	--enable-bcmath \
+	--enable-exif \
+	--with-openssl \
+	--with-curl=/usr \
+	--disable-cgi \
+	--enable-gd-native-ttf \
+	--with-mcrypt \
+    --without-sqlite3 \
+    --without-pdo-sqlite \
+    --with-jpeg-dir=/usr \
+    --with-png-dir=/usr
+make && make install
+cd .. && rm -rf php-5.6.28 php-5.6.28.tar.gz
