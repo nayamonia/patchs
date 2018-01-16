da9bc7f04b16aac38280ed13cba7d920cd080c53
Gabriel Fernandes <gabriel@duel.com.br>
2018-01-16 14:13:24 -0200

    update centos7-redirect-ssl-to-9443.sh

--- centos7-redirect-ssl-to-9443.sh
+++ centos7-redirect-ssl-to-9443.sh
@@ -3,4 +3,5 @@
 # Author: Gabriel Fernandes - gabriel@cd2.com.br
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 9443
 service iptables save
-service iptables restart
+systemctl restart iptables 
+systemctl enable iptables
