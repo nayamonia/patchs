699a8e4b1d7b855cd545b3bbf1ebad1a3b7186fe
Gabriel Fernandes <gabriel@duel.com.br>
2018-02-20 12:13:19 -0300

    mysql root from any address granta

--- centos7-redirect-port-80-and-443.sh
+++ centos7-redirect-port-80-and-443.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Permanent redirect 443 to 9443 on CentOS Linux release 7.3.1611 (Core)
+# Permanent redirect 80 to 9080 on CentOS Linux release 7.3.1611 (Core)
 # Author: Gabriel Fernandes - gabriel@cd2.com.br
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 9443
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 9080
